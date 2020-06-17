0ff20658f72bf90acbe8391290ea7f9f475849ce
Callum <callum.git@outlook.com>
2020-06-17 11:19:34 +0100

    Tidied up card grid, edited timeline data

--- src/components/layout/Portfolio.js
+++ src/components/layout/Portfolio.js
@@ -8,15 +8,17 @@ export default function Portfolio() {
         return (<div className="portfolio-item" key={item.id}>
             <div className="card">
                 <img className="card-img-top" src={require(`../../assets/img/${item.img}.png`)} alt="Portfolio project"></img>
-                <div className="card-title">{item.title}</div>
-                <div className="card-text">{item.description}</div>
-                <div className="btn-group">
-                    <a className="btn btn-primary" href={item.url} target="_blank" rel="noopener noreferrer">View live <i class="las la-external-link-alt"></i></a>
-                    <a className="btn btn-secondary" href={item.github_link} target="_blank" rel="noopener noreferrer">GitHub <i class="lab la-github"></i></a>
+                <div className="card-bottom-content">
+                    <div className="card-title">{item.title}</div>
+                    <div className="card-text">{item.description}</div>
+                    <div className="btn-group">
+                        <a className="btn btn-primary" href={item.url} target="_blank" rel="noopener noreferrer">View live <i class="las la-external-link-alt"></i></a>
+                        <a className="btn btn-secondary" href={item.github_link} target="_blank" rel="noopener noreferrer">GitHub <i class="lab la-github"></i></a>
+                    </div>
+                    <div className="card-tags">{item.tech.map(tech => {
+                        return (<p>{tech} <i className={`lab la-${tech}`}></i></p>)
+                    })}</div>
                 </div>
-                <div className="card-tags">{item.tech.map(tech => {
-                    return (<p>{tech} <i className={`lab la-${tech}`}></i></p>)
-                })}</div>
             </div>
         </div>)
     })
