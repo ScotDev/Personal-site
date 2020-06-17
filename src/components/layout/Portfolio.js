584bb8946506ea6d6847eb58f09148bed57b6c10
Callum <callum.git@outlook.com>
2020-06-17 12:08:03 +0100

    Tidied up card grid, edited timeline data

--- src/components/layout/Portfolio.js
+++ src/components/layout/Portfolio.js
@@ -11,13 +11,16 @@ export default function Portfolio() {
                 <div className="card-bottom-content">
                     <div className="card-title">{item.title}</div>
                     <div className="card-text">{item.description}</div>
-                    <div className="btn-group">
-                        <a className="btn btn-primary" href={item.url} target="_blank" rel="noopener noreferrer">View live <i class="las la-external-link-alt"></i></a>
-                        <a className="btn btn-secondary" href={item.github_link} target="_blank" rel="noopener noreferrer">GitHub <i class="lab la-github"></i></a>
+
+                    <div className="card-btn-tag-group">
+                        <div className="btn-group">
+                            <a className="btn btn-primary" href={item.url} target="_blank" rel="noopener noreferrer">View live <i class="las la-external-link-alt"></i></a>
+                            <a className="btn btn-secondary" href={item.github_link} target="_blank" rel="noopener noreferrer">GitHub <i class="lab la-github"></i></a>
+                        </div>
+                        <div className="card-tags">{item.tech.map(tech => {
+                            return (<p>{tech} <i className={`lab la-${tech}`}></i></p>)
+                        })}</div>
                     </div>
-                    <div className="card-tags">{item.tech.map(tech => {
-                        return (<p>{tech} <i className={`lab la-${tech}`}></i></p>)
-                    })}</div>
                 </div>
             </div>
         </div>)
