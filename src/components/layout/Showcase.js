dae022f0a0b514554b3e182cf969db42d98d65f8
Callum <callum.git@outlook.com>
2020-06-18 18:09:40 +0100

    Finished initial mobile design, removed profile pic/blurb

--- src/components/layout/Showcase.js
+++ src/components/layout/Showcase.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
-import profileImg from '../../assets/img/profile.jpg'
+// import profileImg from '../../assets/img/profile.jpg'
 
 export default function Showcase() {
     return (
         <section id="showcase" className="showcase">
-            <div className="showcase-item">
+            {/* Decided against profile pic and blurb for now */}
+            {/* <div className="showcase-item">
                 <aside className="profile">
                     <img src={profileImg} alt="Profile"></img>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
             </p>
                 </aside>
-            </div>
+            </div> */}
             <div className="showcase-item">
                 <div className="showcase-main">
                     <h1>Hi, I'm Callum Giles</h1>
